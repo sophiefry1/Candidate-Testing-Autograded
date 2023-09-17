f52485310f694d2a1d657b3cf4cafece65680b07
Sophie  Fry <sophiefry@Sophies-MacBook-Air.local>
2023-09-17 16:37:30 -0500

    Phrased name input as question

--- candidate-testing.js
+++ candidate-testing.js
@@ -16,7 +16,7 @@ let candidateAnswers;
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-  candidateName = input.question("Canidate Name: ");
+  candidateName = input.question("What is your name?: ");
 }
 
 function askQuestion() {

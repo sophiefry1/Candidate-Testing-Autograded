a457d5fb832b69e923bbf801c21e8ea35305e5d6
Sophie  Fry <sophiefry@Sophies-MacBook-Air.local>
2023-09-24 17:02:47 -0500

    Part 2 Candidate testing

--- candidate-testing.js
+++ candidate-testing.js
@@ -1,5 +1,4 @@
 const input = require("readline-sync");
-
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName //
@@ -10,8 +9,15 @@ let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
+let questions = [
+  "Who was the first American woman in space? ",
+  "True or false: 5 kilometer == 5000 meters? ",
+  "(5 + 3)/2 * 10 = ? ",
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  "What is the minimum crew size for the ISS? ",
+];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+
 let candidateAnswers;
 
 function askForName() {
